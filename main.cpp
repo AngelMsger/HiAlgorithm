05885661cc74cfea6ad38aec9e587b12bd33ee22
AngelMsger <AngelMsger@Vip.QQ.Com>
2017-03-11 18:11:18 +0800

    Create New Repository.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+int main() {
+    auto start = chrono::high_resolution_clock::now();
+    cout << "Nothing Here!" << endl;
+    auto end = chrono::high_resolution_clock::now();
+    cout << chrono::duration<float, milli>(end - start).count() << endl;
+    return 0;
+}
