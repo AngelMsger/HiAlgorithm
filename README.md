3167312618436dc4b500c6a5c6d4cf1a9857be68
AngelMsger <AngelMsger@Vip.QQ.Com>
2018-08-14 13:50:10 +0800

    Update Solution for 009.

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 > 我现在想成为一名不那么差的程序员。 -- Vista Chyi 2018.8
 
 ## Change Log
+2018/08/14 - Update 007, 009.
 2018/08/13 - Migrate 001, 002, 003, 015 from Original Code.
---
+++ leetcode/009.cpp
@@ -0,0 +1,80 @@
+//
+// Created by angelmsger on 8/14/18.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * 009 - 回文数
+ * @see https://leetcode-cn.com/problems/palindrome-number/description/
+ *
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ *
+ * 示例 1:
+ * 输入: 121
+ * 输出: true
+ *
+ * 示例 2:
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ *
+ * 示例 3:
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ *
+ * 进阶:
+ * 你能不将整数转为字符串来解决这个问题吗？
+ *
+ * 解题思路：字符串版本可以双端为起点遍历，非字符串版本同样有多种实现方式，如007中的数字反转，
+ * 或本题给出的逐位取数。
+ */
+
+class Solution {
+private:
+    bool stringVersion(int x) {
+        auto xStr = to_string(x);
+        auto front = xStr.cbegin(), back = xStr.cend() - 1;
+        while (front < back && *front == *back) {
+            front++;
+            back--;
+        }
+        return front >= back;
+    }
+
+    bool nonStringVersion(int x) {
+        if (x <= 0) {
+            return !x;
+        }
+        vector<int> digits;
+        auto digitsCount = static_cast<int>(log10(x)) + 1;
+        for (auto i = 0; i < digitsCount; ++i) {
+            digits.push_back(x % 10);
+            x /= 10;
+        }
+        auto front = digits.cbegin(), back = digits.cend() - 1;
+        while (front < back && *front == *back) {
+            front++;
+            back--;
+        }
+        return front >= back;
+    }
+public:
+    bool isPalindrome(int x) {
+        return nonStringVersion(x);
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+
+    auto start = chrono::high_resolution_clock::now();
+    auto result = solution.isPalindrome(121);
+    auto end = chrono::high_resolution_clock::now();
+
+    cout << result;
+    cout << "\nin " << chrono::duration<float, milli>(end - start).count() << " ms." << endl;
+
+    return 0;
+}
