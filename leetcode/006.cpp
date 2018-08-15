132d508bd3141d174035e38eb134ce331315b2c6
AngelMsger <AngelMsger@Vip.QQ.Com>
2018-08-15 20:38:01 +0800

    Update Solution for 006.

---
+++ leetcode/006.cpp
@@ -0,0 +1,74 @@
+//
+// Created by angelmsger on 8/15/18.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * 将字符串 "PAYPALISHIRING" 以Z字形排列成给定的行数：
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 之后从左往右，逐行读取字符："PAHNAPLSIIGYIR"
+ * 实现一个将字符串进行指定行数变换的函数:
+ * string convert(string s, int numRows);
+ *
+ * 示例 1:
+ * 输入: s = "PAYPALISHIRING", numRows = 3
+ * 输出: "PAHNAPLSIIGYIR"
+ *
+ * 示例 2:
+ * 输入: s = "PAYPALISHIRING", numRows = 4
+ * 输出: "PINALSIGYAHRPI"
+ *
+ * 解释:
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ *
+ * 解题思路：按照规则逐步构造多个队列并顺序输出即可。
+ */
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows < 2) {
+            return s;
+        }
+        const auto length = s.size();
+        auto bucketSize = numRows * 2 - 2;
+        auto bucketsCount = length % bucketSize == 0 ? length / bucketSize : length / bucketSize + 1;
+        vector<vector<char>> rows((unsigned long) numRows);
+        for (auto i = 0; i < bucketsCount; ++i) {
+            auto j = 0, k = i * bucketSize;
+            while (j < numRows && k < length) {
+                rows[j].push_back(s[k]);
+                ++j, ++k;
+            }
+            j -= 2;
+            while (j > 0 && k < length) {
+                rows[j].push_back(s[k]);
+                --j, ++k;
+            }
+        }
+        ostringstream oss;
+        for (const vector<char> &row: rows) {
+            copy(row.cbegin(), row.cend(), ostream_iterator<char>(oss));
+        }
+        return oss.str();
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+
+    auto start = chrono::high_resolution_clock::now();
+    auto result = solution.convert("", 6);
+    auto end = chrono::high_resolution_clock::now();
+
+    cout << result;
+    cout << "\nin " << chrono::duration<float, milli>(end - start).count() << " ms." << endl;
+
+    return 0;
+}
